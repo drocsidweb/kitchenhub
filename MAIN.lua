31654ccc33d4d2162f8dc923729e7ace44ab15e0
waterweb <waterweb@users.noreply.github.com>
2022-03-27 16:56:14 +0200

    OBF Clicker Sim + Main Updated
    i have clicker sim source if u wanna continue coding on that

--- MAIN.lua
+++ MAIN.lua
@@ -1,5 +1,7 @@
 if game.PlaceId == 286090429 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/waterweb/kitchenhub/main/Arsenal.lua?token=GHSAT0AAAAAABS5SC5VZO4HZF26RLZHEGJ4YSAO73A"))();
+    -- Arsenal
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/waterweb/kitchenhub/main/Arsenal.lua"))();
 elseif game.PlaceId == 7560156054 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/waterweb/kitchenhub/main/ClickerSimulator.lua?token=GHSAT0AAAAAABS5SC5UMSYXBYPCVVMTTPLAYSAPAVA"))();
+    -- Clicker Simulator
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/waterweb/kitchenhub/main/ClickerSimulator.lua"))();
 end
