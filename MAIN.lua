f4b2778d3d348c306b5b91559338543ed70dd97f
waterweb <waterweb@users.noreply.github.com>
2022-03-27 16:42:47 +0200

    Update MAIN.lua

--- MAIN.lua
+++ MAIN.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 286090429 then
-    require("Arsenal")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/waterweb/kitchenhub/main/Arsenal.lua?token=GHSAT0AAAAAABS5SC5VZO4HZF26RLZHEGJ4YSAO73A"))();
 elseif game.PlaceId == 7560156054 then
-    require("ClickerSimulator")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/waterweb/kitchenhub/main/ClickerSimulator.lua?token=GHSAT0AAAAAABS5SC5UMSYXBYPCVVMTTPLAYSAPAVA"))();
 end
